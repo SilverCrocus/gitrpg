62ffc20a1a03e964334f6ce6656bd9928be67e34
Silver Crocus <2muchhivinlogic@gmail.com>
2026-01-04 16:06:21 +1100

    feat: add CharacterSprite index export

---
+++ dashboard/src/components/CharacterSprite/index.ts
@@ -0,0 +1,8 @@
+export { CharacterSprite } from './CharacterSprite';
+export type {
+  CharacterSpriteProps,
+  CharacterClass,
+  AnimationName,
+  AnimationConfig,
+} from './types';
+export { ANIMATION_CONFIG } from './types';
